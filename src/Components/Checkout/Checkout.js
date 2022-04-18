0eba3e433df6ebbff1349fc53927c6bf231ab5e9
leonsabirwm <leonsabirwm@gmail.com>
2022-04-18 17:44:04 +0600

    refactoring in checkout page

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -6,23 +6,23 @@ const Checkout = () => {
         <div className='text-white'>
            <div className='checkout-form'>
               <div>
-              <h4 className=''>Personal Info</h4>
-               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='Your Name' />
+              <h4 className=''>Client Info</h4>
+               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='Your Name' required/>
                <br />
-               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='You Phone' />
+               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='You Phone' required />
                <br />
-               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='You Email' />
+               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='You Email'  required/>
               </div>
            </div>
            <div className='billing-address'>
                <h4>Billing Address</h4>
-               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='City' />
+               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='City' required />
                <br />
-               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='State' />
+               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='State' required />
                <br />
-               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='Zip Code' />
+               <input className='border-0 outline-0 w-25 mb-4' type="text" placeholder='Zip Code'  required/>
                <br />
-               <textarea className='border-0 outline-0 w-25 mb-4' placeholder='Your Address'></textarea>
+               <textarea className='border-0 outline-0 w-25 mb-4' placeholder='Your Address' required></textarea>
            </div>
            <div>
                <button className='bg-danger border-0 px-3 py-2'>Proceed Checkout</button>
